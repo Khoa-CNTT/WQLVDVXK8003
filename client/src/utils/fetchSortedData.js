65d59ed09b96f29dfb03c6804d01f7565e38710e
Phong Thanh <tangthanh.phong99@gmail.com>
2025-05-23 11:24:06 +0700

    refactor fnc fetchSortData

--- client/src/utils/fetchSortedData.js
+++ client/src/utils/fetchSortedData.js
@@ -1,13 +1,22 @@
-export async function fetchSortedData(api, url) {
+function extractData(response) {
+  let data = response?.data;
+  while (data && typeof data === 'object' && 'data' in data) {
+    data = data.data;
+  }
+  return Array.isArray(data) ? data : [];
+}
+/**
+ * Lấy và sắp xếp dữ liệu theo ID giảm dần từ một endpoint
+ * @param {object} api - Đối tượng API đã cấu hình
+ * @param {string} url - Đường dẫn endpoint
+ * @param {function} [mapper] - Hàm tùy chọn để biến đổi từng item
+ */
+export async function fetchSortedData(api, url, mapper = (item) => ({ id: item.id, ...item })) {
   try {
     const response = await api.get(url);
-    const rawData = response?.data?.data.data || [];
+    const rawData = extractData(response);
 
-    const mappedData = rawData.map(item => ({
-      id: item.id,
-      ...item,
-    }));
-
+    const mappedData = rawData.map(mapper);
     mappedData.sort((a, b) => b.id - a.id);
 
     return mappedData;
